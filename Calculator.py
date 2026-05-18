d8a062777833e3af65240cf33e20c833e226f9a7
Madhuri-Chavan-12 <madhurichavan012@gmail.com>
2026-05-18 10:22:11 +0530

    Added changes1 in calculator program

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,12 @@
-# Simple Calculator Program
+# Calculator Program
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = 10
+num2 = 5
 
-print("Choose operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+print("First Number =", num1)
+print("Second Number =", num2)
 
-choice = input("Enter choice (1/2/3/4): ")
-
-if choice == "1":
-    print("Result =", num1 + num2)
-
-elif choice == "2":
-    print("Result =", num1 - num2)
-
-elif choice == "3":
-    print("Result =", num1 * num2)
-
-elif choice == "4":
-    if num2 != 0:
-        print("Result =", num1 / num2)
-    else:
-        print("Division by zero not allowed")
-
-else:
-    print("Invalid choice")
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
